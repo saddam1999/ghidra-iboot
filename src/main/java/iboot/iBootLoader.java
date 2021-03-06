5e3e60a1ba1a73f8eb11eb7577eecc400b7ce6f5
ExhoAR22 <exhoar@gmail.com>
2021-03-06 17:08:51 +0200

    Image type, and board/soc identified

--- src/main/java/iboot/iBootLoader.java
+++ src/main/java/iboot/iBootLoader.java
@@ -9,8 +9,10 @@ import ghidra.app.util.importer.MessageLog;
 import ghidra.app.util.opinion.AbstractLibrarySupportLoader;
 import ghidra.app.util.opinion.LoadSpec;
 import ghidra.framework.model.DomainObject;
+import ghidra.program.model.lang.LanguageCompilerSpecPair;
 import ghidra.program.model.listing.Program;
 import ghidra.util.exception.CancelledException;
+import ghidra.util.exception.InvalidInputException;
 import ghidra.util.task.TaskMonitor;
 
 public class iBootLoader extends AbstractLibrarySupportLoader {
@@ -22,7 +24,15 @@ public class iBootLoader extends AbstractLibrarySupportLoader {
 	@Override
 	public Collection<LoadSpec> findSupportedLoadSpecs(ByteProvider provider) throws IOException {
 		var result = new ArrayList<LoadSpec>();
-		iBootVersion version = new iBootVersion(provider);
+		try {
+			iBootVersion version = new iBootVersion(provider);
+			if (version.isSupported()) {
+				result.add(new LoadSpec(this, version.getBaseAddress(),
+						new LanguageCompilerSpecPair("", "default"), true));
+			}
+		} catch (InvalidInputException exception) {
+
+		}
 		return result;
 	}
 
--- src/main/java/iboot/iBootVersion.java
+++ src/main/java/iboot/iBootVersion.java
@@ -1,9 +1,11 @@
 package iboot;
 
 import ghidra.app.util.bin.ByteProvider;
+import ghidra.util.exception.InvalidInputException;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 public class iBootVersion {
     private static final long DESCRIPTION_OFFSET = 0x200;
@@ -18,7 +20,60 @@ public class iBootVersion {
     };
 
     private static final String[] DEVICES_32BIT = new String[] {
+            // iPhone
+            "m68ap", // iPhone 2G (1,1)
+            "n82ap", // iPhone 3G (1,2)
+            "n88ap", // iPhone 3GS (2,1)
+            "n90ap", // iPhone 4 (3,1)
+            "n90bap", // iPhone 4 (3,2)
+            "n92ap", // iPhone 4 (3,3)
+            "n94ap", // iPhone 4S (4,1)
+            "n41ap", // iPhone 5 (5,1)
+            "n42ap", // iPhone 5 (5,2)
+            "n48ap", // iPhone 5c (5,3)
+            "n49ap", // iPhone 5c (5,4)
 
+            // iPod touch
+            "n45ap", // iPod touch 1st generation (1,1)
+            "n72ap", // iPod touch 2nd generation (2,1)
+            "n18ap", // iPod touch 3rd generation (3,1)
+            "n81ap", // iPod touch 4th generation (4,1)
+            "n78ap", // iPod touch 5th generation (5,1)
+            "n78aap", // iPod touch 5th generation (5,1)
+
+            // iPad
+            "k48ap", // iPad 1st generation (1,1)
+            "k93ap", // iPad 2 (2,1)
+            "k94ap", // iPad 2 (2,2)
+            "k95ap", // iPad 2 (2,3)
+            "k93aap", // iPad 2 (2,4)
+            "p105ap", // iPad mini (2,5)
+            "p106ap", // iPad mini (2,6)
+            "p107ap", // iPad mini (2,7)
+            "j1ap", // iPad 3rd generation (3,1)
+            "j2ap", // iPad 3rd generation (3,2)
+            "j2aap", // iPad 3rd generation (3,3)
+            "p101ap", // iPad 4th generation (3,4)
+            "p102ap", // iPad 4th generation (3,5)
+            "p103ap", // iPad 4th generation (3,6)
+
+            // Apple TV
+            "k66ap", // Apple TV 2nd generation (2,1)
+            "j33ap", // Apple TV 3rd generation (3,1)
+            "j33iap", // Apple TV 3rd generation (3,2)
+
+            // SoCs
+            "s5l8900xsi", // ???
+            "s5l8720xsi", // ???
+            "s5l8920xsi", // ???
+            "s5l8922xsi", // ???
+            "s5l8930xsi", // A4
+            "s5l8940xsi", // A5
+            "s5l8942xsi", // A5
+            "s5l8945xsi", // A5X
+            "s5l8947xsi", // A5
+            "s5l8950xsi", // A6
+            "s5l8955xsi", // A6X
     };
 
     private final String description;
@@ -28,21 +83,31 @@ public class iBootVersion {
                 StandardCharsets.US_ASCII);
     }
 
-    public String getType() {
+    public String getType() throws InvalidInputException {
         for (String type : TYPES) {
             if (description.startsWith(type + " for ")) {
                 return type;
             }
         }
-        return "";
+        throw new InvalidInputException();
     }
 
-    public String getDevice() {
+    public String getDevice() throws InvalidInputException {
         String descriptionWithoutType = this.description.substring((this.getType() + " for ").length());
         return descriptionWithoutType.substring(0, descriptionWithoutType.indexOf(',')).toLowerCase();
     }
 
-    public int getBitness() {
+    public boolean isSupported() throws InvalidInputException {
+        String device = this.getDevice();
+        for (String unsupportedDevice : DEVICES_32BIT) {
+            if (device.equals(unsupportedDevice)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public long getBaseAddress() {
         return 0;
     }
 }
